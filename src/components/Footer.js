e79a5b4cb351b688779628cc5cc572fd08cd0f84
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-15 21:01:40 +0300

    adding code footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer>
-            
+            <small>&#169;2022 Coding with &#x2764; by Dev. Mai Elhajeen | all rights reserved </small>
         </footer>
     )
 }
