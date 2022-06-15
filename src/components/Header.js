de8b601a52188b3e7249d1d8a4f126b7d7c32a55
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-15 10:54:14 +0300

    adding home content in home section

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,7 +48,10 @@ function Header() {
                 </div>
 
                 <div className='home-content'>
-
+                    <h1>Online Experiences</h1>
+                    <p>
+                        Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                    </p>
                 </div>
             </div> 
         </header>
