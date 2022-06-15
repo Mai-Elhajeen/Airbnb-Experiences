18beeab3ac9bb020fa3419a018e4a2c5c91ac807
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-15 10:52:19 +0300

    adding img section in home section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import Logo from '../img/airbnb-logo.svg'
+// Images
+import Img1 from '../img/image1.png'
+import Img2 from '../img/image.png'
+import Img3 from '../img/image3.png'
+import Img4 from '../img/image4.png'
+import Img5 from '../img/image5.png'
+import Img6 from '../img/image6.png'
+import Img7 from '../img/image7.png'
+import Img8 from '../img/image8.png'
+import Img9 from '../img/image9.png'
+
+
 function Header() {
     return (
         // start header section 
@@ -9,7 +21,35 @@ function Header() {
             </nav> 
 
             <div className='home' id='home'>
-                
+                <div className='home-img'>
+                    <div className='div-img single'>
+                        <img src={Img1} alt='' />
+                    </div>
+
+                    <div className='div-img up'>
+                        <img src={Img2} alt='' />
+                        <img src={Img3} alt='' />
+                    </div>
+
+                    <div className='div-img down'>
+                        <img src={Img4} alt='' />
+                        <img src={Img5} alt='' />
+                    </div>
+
+                    <div className='div-img up'>
+                        <img src={Img6} alt='' />
+                        <img src={Img7} alt='' />
+                    </div>
+
+                    <div className='div-img down'>
+                        <img src={Img8} alt='' />
+                        <img src={Img9} alt='' />
+                    </div>
+                </div>
+
+                <div className='home-content'>
+
+                </div>
             </div> 
         </header>
     )
