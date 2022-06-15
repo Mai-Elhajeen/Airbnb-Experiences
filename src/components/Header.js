e1ed51013e507471a437bc14f3cfc58b3bb08ebe
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-15 10:13:01 +0300

    adding navbar code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import Logo from '../img/airbnb-logo.svg'
 function Header() {
     return (
         // start header section 
         <header> 
             <nav className='navbar'>
-
+                <img src={Logo} alt='logo' />
             </nav> 
 
             <div className='home' id='home'>
-
+                
             </div> 
         </header>
     )
