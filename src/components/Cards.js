f8ad01d15a83def671f016b807fc5fafcad8f278
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-24 18:37:03 +0300

    prapering the file to pass props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function Cards() {
-  return (
-    <div>Cards</div>
-  )
+    return (
+        <div className='card'>
+            <div className='img-card'>
+                <img src='' alt='' />
+                <button>SOLD OUT</button>
+            </div>
+
+            <div className='card-content'>
+                <div className='rating'>
+                    <i class="ri-star-fill"></i>
+                    <span className='num'>5.0</span>
+                    <span className='rate'>(6).USA</span>
+                </div>
+                <p>
+                    Life lessons with Katie Zaferes
+                </p>
+                <div className='salary'>
+                    From $136 / <span>person</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Cards
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,15 @@
 import React from 'react'
-import ImgCard1 from '../img/card1.png'
-import ImgCard2 from '../img/card2.png'
-import ImgCard3 from '../img/card3.png'
+import Cards from './Cards'
+// import ImgCard1 from '../img/card1.png'
+// import ImgCard2 from '../img/card2.png'
+// import ImgCard3 from '../img/card3.png'
 
 function Main() {
     return (
         <main>
-            <div className='card'>
-                <div className='img-card'>
-                    <img src={ImgCard1} alt='' />
-                    <button>SOLD OUT</button>
-                </div>
-
-                <div className='card-content'>
-                    <div className='rating'>
-                        <i class="ri-star-fill"></i>
-                        <span className='num'>5.0</span>
-                        <span className='rate'>(6).USA</span>
-                    </div>
-                    <p>
-                        Life lessons with Katie Zaferes
-                    </p>
-                    <div className='salary'>
-                        From $136 / <span>person</span>
-                    </div>
-                </div>
-            </div>
+            <Cards />
+            <Cards />
+            <Cards />
         </main>
     )
 }
